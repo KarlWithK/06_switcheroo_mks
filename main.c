3aedec42bdb2f4782b138fdf73c8cdeadbaaa6b3
KarlWithK <jocelinc60@outlook.com>
2020-10-14 09:11:00 -0400

    Fixed indentation

--- main.c
+++ main.c
@@ -10,15 +10,15 @@ int main() {
   }
   arr[9] = 0;
   for (i = 0; i < 10; i++) {
-    printf("%d\n", arr[i]);
+    printf("Index: %d with num: %d\n", i, arr[i]);
   }
-
   int copy[10];
   int *cap = copy;
   int *arrp = arr + 9;
+  printf("Reverse Array\n");
   for (i = 0; i < 10; i++) {
     *cap = *arrp;
-    printf("%d\n", *cap);
+    printf("index: %d with num: %d\n", i, *cap);
     cap++;
     arrp--;
   }
